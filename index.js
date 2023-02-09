457a265d81bad65e1ea4c5b8be157ceddd1cb32d
los-ilusos-awards <124903816+los-ilusos-awards@users.noreply.github.com>
2023-02-09 23:20:15 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,21 +1,19 @@
-Window.alert('Para continuar tienes que aceptar los terminos y condiciones (no hay xd pero acepta igual)')
 
-/* ésto comprueba la localStorage si ya tiene la variable guardada */
-function compruebaAceptaCookies() {
-  if(localStorage.aceptaCookies != 'true'){
-    cajacookies.style.display = 'block';
-  }
+const botonAceptarCookies = document.getElementById('btn-aceptar-cookies');
+const avisoCookies = document.getElementById('aviso-cookies');
+const fondoAvisoCookies = document.getElementById('fondo-aviso-cookies');
+
+
+if(!localStorage.getItem('cookies-aceptadas')){
+    avisoCookies.classList.add('activo');
+    fondoAvisoCookies.classList.add('activo');
 }
 
-/* aquí guardamos la variable de que se ha
-aceptado el uso de cookies así no mostraremos
-el mensaje de nuevo */
-function aceptarCookies() {
-  localStorage.aceptaCookies = 'true';
-  cajacookies.style.display = 'none';
-}
 
-/* ésto se ejecuta cuando la web está cargada */
-$(document).ready(function () {
-  compruebaAceptaCookies();
-});
+
+botonAceptarCookies.addEventListener('click', () => {
+    avisoCookies.classList.remove('activo')
+    fondoAvisoCookies.classList.remove('activo')
+
+    localStorage.setItem('cookies-aceptadas', true)
+})
